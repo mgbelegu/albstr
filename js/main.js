48fe93edab7d012c52bc8d3bfa99951272aa7002
Marti Belegu <48526054+mgbelegu@users.noreply.github.com>
2020-12-07 15:00:04 +0100

    Fixed padding

--- js/main.js
+++ js/main.js
@@ -6,3 +6,54 @@ $(document).ready(function () {
     }
   });
 });
+
+/*var homeOffset = $("#home").offset().top;
+var aboutOffset = $("#about").offset().top;
+var servicesOffset = $("#services").offset().top;
+var projectsOffset = $("#projects").offset().top;
+var contactOffset = $("#contact").offset().top;
+
+var $w = $(window).scroll(function () {
+  if ($w.scrollTop() > homeOffset) {
+    if ($("#navHome").hasClass("active") === false) {
+      $(this).addClass("active");
+      $("#navAbout").removeClass("active");
+      $("#navServices").removeClass("active");
+      $("#navProjects").removeClass("active");
+      $("#navContact").removeClass("active");
+    }
+  } else if ($w.scrollTop() > aboutOffset) {
+    if ($("#navAbout").hasClass("active") === false) {
+      $(this).addClass("active");
+      $("#navHome").removeClass("active");
+      $("#navServices").removeClass("active");
+      $("#navProjects").removeClass("active");
+      $("#navContact").removeClass("active");
+    }
+  } else if ($w.scrollTop() > servicesOffset) {
+    if ($("#navServices").hasClass("active") === false) {
+      $(this).addClass("active");
+      $("#navAbout").removeClass("active");
+      $("#navHome").removeClass("active");
+      $("#navProjects").removeClass("active");
+      $("#navContact").removeClass("active");
+    }
+  } else if ($w.scrollTop() > projectsOffset) {
+    if ($("#navProjects").hasClass("active") === false) {
+      $(this).addClass("active");
+      $("#navAbout").removeClass("active");
+      $("#navServices").removeClass("active");
+      $("#navHome").removeClass("active");
+      $("#navContact").removeClass("active");
+    }
+  } else if ($w.scrollTop() > contactOffset) {
+    if ($("#navContact").hasClass("active") === false) {
+      $(this).addClass("active");
+      $("#navAbout").removeClass("active");
+      $("#navServices").removeClass("active");
+      $("#navProjects").removeClass("active");
+      $("#navHome").removeClass("active");
+    }
+  }
+});
+*/
