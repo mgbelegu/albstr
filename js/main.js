53ac44406b2855de33d66859839d84866aa7f787
Marti Belegu <48526054+mgbelegu@users.noreply.github.com>
2020-12-08 10:16:25 +0100

    AOS, stats counter

--- js/main.js
+++ js/main.js
@@ -55,6 +55,23 @@ $(document).ready(function () {
       }
     });
   });
+
+  $(".counter-count").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 5000,
+          easing: "swing",
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          },
+        }
+      );
+  });
 });
 
 /*var homeOffset = $("#home").offset().top;
